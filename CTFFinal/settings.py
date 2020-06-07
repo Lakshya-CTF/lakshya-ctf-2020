733a8fc1fd9059f3088cab401985fb56e8d3b144
chaitanyarahalkar <chaitanyarahalkar4@gmail.com>
2020-06-07 16:39:38 +0530

    Update password reset form contents

--- CTFFinal/settings.py
+++ CTFFinal/settings.py
@@ -132,23 +132,25 @@ elif MODE == 'production':
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-	{
-		"NAME":
-		"django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-	},
-	{
-		"NAME":
-		"django.contrib.auth.password_validation.MinimumLengthValidator",
-	},
-	{
-		"NAME":
-		"django.contrib.auth.password_validation.CommonPasswordValidator",
-	},
-	{
-		"NAME":
-		"django.contrib.auth.password_validation.NumericPasswordValidator",
-	},
+	# {
+	# 	"NAME":
+	# 	"django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+	# },
+	# {
+	# 	"NAME":
+	# 	"django.contrib.auth.password_validation.MinimumLengthValidator",
+	# },
+	# {
+	# 	"NAME":
+	# 	"django.contrib.auth.password_validation.CommonPasswordValidator",
+	# },
+	# {
+	# 	"NAME":
+	# 	"django.contrib.auth.password_validation.NumericPasswordValidator",
+	# },
 ]
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
