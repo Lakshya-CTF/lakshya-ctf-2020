6b6c4be149cd59e1bf38a42c30be0f546d3bbd59
chaitanyarahalkar <chaitanyarahalkar4@gmail.com>
2020-06-11 12:15:04 +0530

    Fix profile issue

--- app/views.py
+++ app/views.py
@@ -92,7 +92,6 @@ def profile(request,username):
 	if rank == 0:
 		rank = 1
 
-	challenges_solved = SolvedChallenges.objects.filter(user = user).count()
 	root_owns = SolvedMachines.objects.filter(user = user,root=True).count()
 	timestamps = SolvedTimestamps.objects.filter(username = user)
 	vals = SolvedQuestions.objects.filter(user = user)
@@ -104,7 +103,7 @@ def profile(request,username):
 
 	stats = list(stats_dict.values())
 
-	return render(request,"app/profile.html",{"user":user,"challenges_solved":challenges_solved,"root_owns":root_owns,"timestamps":timestamps,"stats":stats,"rank":rank})
+	return render(request,"app/profile.html",{"user":user,"challenges_solved":len(vals),"root_owns":root_owns,"timestamps":timestamps,"stats":stats,"rank":rank})
 
 def index(request):
 	return render(request, "app/index.html")
